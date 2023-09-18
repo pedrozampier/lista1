3d3245706ea7c79b43e2a24fcdf6e240f89e5afe
pedro <pedrootaviopz@gmail.com>
2023-09-18 00:31:25 -0300

    exericios 1, 3, 5, 7, 9, 11

--- MetodosDeArray.java
+++ MetodosDeArray.java
@@ -1,4 +1,5 @@
 package lista1;
+import java.util.Random;
 import java.util.Arrays;
 
 public class MetodosDeArray{
@@ -43,13 +44,128 @@ public class MetodosDeArray{
         return isOrdened;
     }
 
-    
+    public String bubbleSortMelhorado(int[] array) {
+        int tamanhoDaArray = array.length;
+        boolean trocou;
+        int[] sortedArr = new int[tamanhoDaArray];
+
+        for (int i = 0; i < tamanhoDaArray; i++) {
+            sortedArr[i] = array[i];
+        }
+
+        for (int i = 0; i < tamanhoDaArray - 1; i++) {
+            trocou = false;
+
+            for (int j = 0; j < tamanhoDaArray - i - 1; j++) {
+                if (sortedArr[j] > sortedArr[j + 1]) {
+                    int temp = sortedArr[j];
+                    sortedArr[j] = sortedArr[j + 1];
+                    sortedArr[j + 1] = temp;
+                    trocou = true;
+                }
+            }
+            if (!trocou) {
+                break;
+            }
+        }
+        String aux = Arrays.toString(sortedArr);
+        return aux;
+    }
+
+    public String megaSena(int[] arrayDeDezenas){
+
+        int [] numerosSorteados = {10,12,42,65,78,14,98,16,76,43};
+        int quantidadeDeAcertos = 0;
+
+        for(int i = 0; i < arrayDeDezenas.length; i++){
+            for(int j = 0; j < numerosSorteados.length; j++){
+                if(arrayDeDezenas[i] == numerosSorteados[j]){
+                    quantidadeDeAcertos++;
+                }
+            }
+        }
+
+        String tipoDeAcerto = "Não foi dessa vez";
+
+        if(quantidadeDeAcertos == 4){
+            tipoDeAcerto = "Quadra";
+        }if(quantidadeDeAcertos >= 5 && quantidadeDeAcertos < 10){
+            tipoDeAcerto = "Quina";
+        }if(quantidadeDeAcertos == 10){
+            tipoDeAcerto = "Sena!!";
+        }
+
+        return tipoDeAcerto;
+    }
+
+
+    public void gerarCartela() {
+        int[] cartela = new int[25];
+        Random random = new Random();
+
+        for (int i = 0; i < 25; i++) {
+            cartela[i] = 0;
+        }
+
+        for (int i = 0; i < 25; i++) {
+            int numero;
+            boolean repetido;
+
+            do {
+                repetido = false;
+                numero = random.nextInt(50) + 1;
+
+                for (int j = 0; j < i; j++) {
+                    if (cartela[j] == numero) {
+                        repetido = true;
+                        break;
+                    }
+                }
+            } while (repetido);
+
+            cartela[i] = numero;
+        }
+
+        System.out.println("Cartela de Bingo:");
+        for (int i = 0; i < 25; i++) {
+            System.out.print(cartela[i]);
+            if ((i + 1) % 5 == 0) {
+                System.out.println();
+            } else {
+                System.out.print("\t");
+            }
+        }
+}
+        public String gnomeSort(int[] array) {
+        int tamanho = array.length;
+        int index = 0;
+
+        while (index < tamanho) {
+            if (index == 0) {
+                index++;
+            }
+            if (array[index] >= array[index - 1]) {
+                index++;
+            } else {
+                int aux = array[index];
+                array[index] = array[index - 1];
+                array[index - 1] = aux;
+                index--;
+            }
+        }
+
+        String temp = Arrays.toString(array);
+        return temp;
+    }
+
 
     public static void main(String[] args) {
         
         MetodosDeArray metodos = new MetodosDeArray();
-        int[] array = {1,5,3,4};
+        int[] array = {64, 34, 25, 12, 22, 11, 90};
 
-        System.out.println(metodos.isOrdened(array));
+        System.out.println(metodos.gnomeSort(array));
+        metodos.gerarCartela();
     }
 } 
+
